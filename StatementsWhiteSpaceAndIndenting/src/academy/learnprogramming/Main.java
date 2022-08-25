1e710e974e58b9b6df302a2c975d19c28ecd5a0d
Sean Jahangiri <shayan-j@hotmail.com>
2022-08-25 14:13:46 -0700

    Add files via upload

---
+++ StatementsWhiteSpaceAndIndenting/src/academy/learnprogramming/Main.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is " + "another " + "still more.");
+
+        int anotherVariable = 50;
+    }
+}
